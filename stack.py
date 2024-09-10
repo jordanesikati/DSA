68d01371f5045f68b04e5d77c76dcad92b432525
jordanesikati <jordanesikati@gmail.com>
2024-09-10 13:01:57 -0400

    Updated Stack

--- stack.py
+++ stack.py
@@ -1,8 +1,8 @@
-class Stack:
+class ArrayStack:
     def __init__(self):
         self.stack = []
 
-    def size(self):
+    def get_size(self):
         return len(self.stack)
 
     def is_empty(self):
@@ -16,3 +16,36 @@ class Stack:
 
     def peek(self):
         return "Empty" if self.is_empty() else self.stack[-1]
+
+
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+
+
+class LinkedListStack:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def get_size(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def push(self, data):
+        self.head = Node(data, self.head)
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty(): return 'Empty'
+        data = self.head.data
+        self.head = self.head.next
+        self.size -= 1
+        return data
+
+    def peek(self):
+        return 'Empty' if self.is_empty() else self.head.data
+
